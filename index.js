1211fd8164001c9a7b926d14beae7d5135b13b92
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2024-03-12 11:55:28 -0400

    Create toString function

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ function LinkedList() {
     // Set the head to the new value and next node to the head
     const prepend = (value) => {
         list = Node(value, nextNode = list);
+        return list;
     }
 
     const size = () => {
@@ -65,11 +66,24 @@ function LinkedList() {
     }
 
     const at = (index) => {
+        let currentNode = list;
+        // Loop through index amount of times and go to next node until it reaches the index
+        for (let i = 0; i < index; i++) {
+            if (currentNode == null) return null;
+            currentNode = currentNode.nextNode;
+        }
 
+        return currentNode;
     }
 
     const pop = () => {
+        let currentNode = list;
+        while (currentNode.nextNode != null) {
 
+            currentNode = currentNode.nextNode;
+        }
+
+        currentNode.nextNode = null;
     }
 
     const contains = (value) => {
@@ -88,7 +102,32 @@ function LinkedList() {
 
     }
 
-    return { list, append, prepend, tail, size, contains }
+    const find = (value) => {
+        let index = 0;
+        let currentNode = list;
+        while (currentNode != null) {
+            if (currentNode.value == value) {
+                return index;
+            }
+            index++;
+            currentNode = currentNode.nextNode;
+        }
+
+        return null;
+    }
+
+    const toString = () => {
+        let currentNode = list;
+        let listString = '';
+        while (currentNode != null) {
+            listString += `(${currentNode.value}) -> `
+            currentNode = currentNode.nextNode;
+        }
+
+        return listString + null;
+    }
+
+    return { list, append, prepend, tail, size, contains, at, pop, find, toString }
 }
 
 // Factory function for a node that returns a value and nextNode which are null by default
@@ -98,9 +137,10 @@ function Node(value = null, nextNode = null) {
 
 let list1 = LinkedList();
 list1.append('Something');
-list1.prepend('New head');
-list1.append('Something else');
-console.log(list1.size());
+console.log(list1.prepend('New head'));
+console.log(list1.find("Something"));
+console.log(list1.toString());
+
 
 
 let sampleList = {
